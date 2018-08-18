2d2c73296747c5a6f0fb6ae66589f5342bd9ae88
NajimBeam <42489949+NajimBeam@users.noreply.github.com>
2018-08-18 14:30:09 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,70 @@
+
+
+import numpy as np
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+
+
+def get_data_stats(df):
+
+    df.info()
+
+    df['3D ROAS'].mean()
+
+    df[['3D ROAS','30D ROAS']].corr()
+
+    df[['3D ROAS','30D ROAS']][df['Spend in USD']>100].corr()
+
+    plt.style.use('bmh')
+    df.hist(column = 'Spend in USD', bins = np.arange(0,16), color='darkblue')
+    df.corr()['30D ROAS']
+
+
+def load_data():
+
+    df_train = pd.read_csv('datasets/train.csv', encoding='ISO-8859-1', sep=";")
+
+    get_data_stats(df_train)
+
+    df_test = pd.read_csv('datasets/test.csv', encoding='ISO-8859-1', sep=";")
+
+    x = df_train[['3D ROAS','7D ROAS']]
+    y = df_train['30D ROAS']
+
+    return x,y,df_train,df_test
+
+
+def train_model(x,y):
+
+    model = sm.OLS(y,x).fit()
+
+    return model
+
+
+def get_prediction(model,x):
+
+    return model.predict(x)
+
+
+def main():
+
+    x,y,df_train,df_test = load_data()
+
+    model = train_model(x,y)
+    predictions = get_prediction(model,x)
+
+    z = df_test[['3D ROAS', '7D ROAS']]
+    predictions2 = model.predict(z)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
